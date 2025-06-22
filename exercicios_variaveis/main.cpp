a3452d928edf092defde901149231adaf99ced4a
Felipe Costa Lisboa <felipe.lisboa@meracare.com.br>
2025-06-22 15:20:15 -0300

    Exercicio de fixação do tipo int

---
+++ exercicios_variaveis/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int NumComputadores = 100;
+    int SerialComputadores = 11111;
+
+    cout << "$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
+    cout << "O numeros de computadores e: " << NumComputadores << endl;
+    cout << "O serial dos computadores e: " << SerialComputadores << endl;
+    cout << "$$$$$$$$$$$$$$$$$$$$$$$$$" << endl;
+
+
+    return 0;
+}
