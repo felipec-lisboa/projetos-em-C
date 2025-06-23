48e5533d5530b4149f2ee37baeaf4f07466cd785
Felipe Costa Lisboa <felipe.lisboa@meracare.com.br>
2025-06-22 21:11:23 -0300

    Operações

--- RecebbendodadosC++/main.cpp
+++ RecebbendodadosC++/main.cpp
@@ -5,16 +5,23 @@ using namespace std;
 int main()
 {
     int Numero1, Numero2;
+    int Soma, Subtracao, multiplicacao, modulo;
+    float divisao;
     cout << "Digite um numero: ";
     cin >> Numero1;
     cout << "Numero 1 = " << Numero1 << endl;
     cout << "Digite o segundo numero: ";
     cin >> Numero2;
-    cout << "Numero 2 = " << Numero2;
-
-    cout << "Digite dois numeros separados por espaco e aperte enter: ";
-    cin >> Numero1 >> Numero2;
-    cout << "Soma = " << (Numero1 + Numero2);
-
+    cout << "Numero 2 = " << Numero2 << endl;
+    Soma = Numero1 + Numero2;
+    cout << "A soma dos dois numeros e: " << Soma << endl;
+    Subtracao = Numero1 - Numero2;
+    cout << "A Subtracao dos dois numeros e: " << Subtracao << endl;
+    multiplicacao = Numero1 * Numero2;
+    cout << "A multiplicacao dos dois numeros e: " << multiplicacao << endl;
+    divisao = Numero1 / Numero2;
+    cout << "A divisao dos dois numeros e: " << divisao << endl;
+    modulo = Numero1 % Numero2;
+    cout << "O resto da divisao dos dois numeros e: " << modulo << endl;
     return 0;
 }
