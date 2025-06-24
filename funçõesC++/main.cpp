497187d55ad11616a7661f23335b39a185d5c876
Felipe Costa Lisboa <felipe.lisboa@meracare.com.br>
2025-06-24 12:49:57 -0300

    Função void

--- funçõesC++/main.cpp
+++ funçõesC++/main.cpp
@@ -10,15 +10,31 @@ int SomaNumeros(int X, int Y){
 }
 
 
+void Mensagem(){
+
+
+    cout << "Mensagem de Marte... Chegamos bem...\n";
+
+}
+
 
 int main()
 {
-    int Numero, x;
+    int Numero, x, RetornoDaFuncao;
+    Mensagem();
     cout << "Digite um numero: " << endl;
     cin >> Numero;
     cout  << "Digite numero que deseja somar: " << endl;
     cin >> x;
     cout <<"A soma deste numero : " << Numero << " " <<"Com o numero " << x << " "<< "Sera: "<<SomaNumeros(Numero, x) << endl;
+
+    RetornoDaFuncao = SomaNumeros(200, -300);
+    cout << RetornoDaFuncao << endl;
+    cout << SomaNumeros(Numero, SomaNumeros(150,1400));
+    system("PAUSE");
+
+
+
 
     return 0;
 }
