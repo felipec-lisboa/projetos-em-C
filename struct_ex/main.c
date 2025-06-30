23cb6a5340573d1adad79fa6967ec09d75666275
Felipe Costa Lisboa <felipe.lisboa@meracare.com.br>
2025-06-29 23:29:20 -0300

    struct em c

--- struct_ex/main.c
+++ struct_ex/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
 struct pessoa{
     char nome[100];
     int idade;
@@ -9,6 +10,8 @@ struct pessoa{
 
 
 }felipe;
+
+
 int main()
 {
     printf("Digite seu nome: ");
@@ -19,6 +22,52 @@ int main()
     scanf("%f", &felipe.altura);
     getchar();
     printf("Voce se chama %s\n Tem %d anos \n Mede %f", felipe.nome,felipe.idade,felipe.altura);
+*/
+
+
+struct alunos
+{
+    char nome[100];
+    int matricula;
+    float nota;
+};
+
+int main()
+{
+    float media;
+    float acumulador = 0;
+
+    struct alunos st_alunos[2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Digite seu nome: ");
+        fgets(st_alunos[i].nome, 100, stdin);
+        st_alunos[i].nome[strcspn(st_alunos[i].nome, "\n")] = 0;
+
+        printf("Digite sua matricula: ");
+        scanf("%d", &st_alunos[i].matricula);
+
+        printf("Digite sua nota: ");
+        scanf("%f", &st_alunos[i].nota);
+
+        getchar();
+        acumulador += st_alunos[i].nota;
+    }
+
+    media = acumulador / 2;
+    printf("A media foi: %.2f\n", media);
+    printf("Alunos com nota acima da média:\n");
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (st_alunos[i].nota > media)
+        {
+            printf("- %s (nota: %.2f)\n", st_alunos[i].nome, st_alunos[i].nota);
+        }
+    }
 
     return 0;
 }
+
+
